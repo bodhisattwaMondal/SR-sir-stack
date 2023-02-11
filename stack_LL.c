5917bfad54f867f96300bf0620bf58a5ed9b317c
Bodhisattwa Mondal <mondal.bodhi2k01@gmail.com>
2023-02-12 00:46:44 +0530

    Added source code of stack using LL

---
+++ stack_LL.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Stack implementation using Linked List
+struct Stack{
+    int element;
+    struct Stack *next;
+};
+
+void push(struct Stack **top, int item){
+    // creating new node 
+    struct Stack *newNode = (struct Stack*)malloc(sizeof(struct Stack));
+    newNode->element = item;
+
+    // pushing new node into the stack 
+    newNode->next = *top;
+    *top = newNode;
+    // printf("Node succesfully inserted!!");
+}
+
+int pop(struct Stack **top){
+    if (*top == NULL)
+        printf("\nStack underflow!!\n");
+    else{
+        struct Stack *temp = *top;
+        int tempVal = temp->element;
+        *top = (*top)->next;
+        free(temp);
+        return tempVal;
+    }
+}
+
+
+int display(struct Stack *top){
+    if (top == NULL)
+        printf("\nCan't display - Empty stack!!\n");
+    else{
+        printf("The stack is:\n");
+        while (top!= NULL)
+        {
+            printf("%d--->",top->element);
+            top = top->next;
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    // Creating empty Stack 
+    struct Stack *top = NULL;
+
+    // Test Case: empty stack 
+    display(top);
+
+    // Test Case : push
+    push(&top, 78);
+    push(&top, 9);
+    push(&top, 48);
+    push(&top, 780);
+    display(top);
+
+    // Test Case : pop 
+    pop(&top);
+    display(top);
+    return 0;
+}
