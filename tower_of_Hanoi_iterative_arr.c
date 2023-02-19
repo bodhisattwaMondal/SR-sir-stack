4adedcf84db490cff59899d44a5f19f0d6d0fd04
Bodhisattwa Mondal <mondal.bodhi2k01@gmail.com>
2023-02-20 00:26:40 +0530

    ADDED following funcionality: +isFull() +isEmpty() +push() +pop() +display()
    Above funcionality will help to push or pop data into from the stack.
    And will display the stack.

--- tower_of_Hanoi_iterative_arr.c
+++ tower_of_Hanoi_iterative_arr.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-struct Stack{
+struct Stack
+{
     int capacity;
     int top;
     int *arr;
 };
 
-struct Stack* createStack(int capacity){
-    struct Stack *stack = (struct Stack*)malloc(sizeof(struct Stack));
+struct Stack *createStack(int capacity)
+{
+    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
     stack->capacity = capacity;
     stack->top = -1;
-    stack->arr = (int*)malloc(sizeof(int) * stack->capacity);
+    stack->arr = (int *)malloc(sizeof(int) * stack->capacity);
     return stack;
 }
 
+int isFull(struct Stack *stack)
+{
+    return (stack->top == stack->capacity - 1);
+}
+
+int isEmpty(struct Stack *stack)
+{
+    return (stack->top == -1);
+}
+
+void push(struct Stack *stack, int element)
+{
+    if (isFull(stack))
+    {
+        printf("Stack Overflow !!");
+        return;
+    }
+    stack->arr[++stack->top] = element;
+}
+
+int pop(struct Stack *stack)
+{
+    if (isEmpty(stack))
+        printf("Stack Underflow !!");
+    else
+        return stack->arr[stack->top--];
+}
+
+void display(struct Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        printf("Stack Underflow !!");
+        return;
+    }
+
+    printf("Stack:\n");
+    for (int i = stack->top; i > -1; i--)
+        printf("%d\n", stack->arr[i]);
+    printf("\n");
+}
+
+// Driver Code
 int main()
 {
-    
+    struct Stack *myStack = createStack(4);
+    push(myStack, 78);
+    push(myStack, 8);
+    push(myStack, 1548);
+    pop(myStack);
+    display(myStack);
     return 0;
 }
